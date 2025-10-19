7a70309329d4391c3eeb2375dc7a8a2a520e5303
dipti-gupta-19 <diptigupta10b6@gmail.com>
2025-10-19 21:21:55 +0530

    data send to server successfully

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,17 +1,41 @@
-
+import {v2 as cloudinary} from 'cloudinary';
+import ProductModel from '../models/productModel.js';
 
 //function for add product
     const addProduct = async (req, res) => {
         try{
             const {name,description,price,category,subCategory,sizes,bestseller} = req.body;
-            const image1=req.files.image1[0];
-            const image2=req.files.image2[0];
-            const image3=req.files.image3[0];
-            const image4=req.files.image4[0];
+            const image1=req.files.image1 && req.files.image1[0];
+            const image2=req.files.image2 && req.files.image2[0];
+            const image3=req.files.image3 && req.files.image3[0];
+            const image4=req.files.image4 && req.files.image4[0];
 
-            console.log(name,description,price,category,subCategory,sizes,bestseller);
-            console.log(image1,image2,image3,image4);
-            res.json({});
+            const images=[image1,image2,image3,image4].filter((item)=> item!==undefined);
+
+            let imageUrl=await Promise.all(
+                images.map(async (item)=>{
+                    let result= await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+                    return result.secure_url;
+                }) 
+            )
+            
+            const productData= {
+                name,
+                description,
+                price:Number(price),
+                images: imageUrl,  
+                category,
+                subCategory,
+                sizes: JSON.parse(sizes),   
+                bestseller: bestseller === 'true' ? true : false,
+                date:Date.now()
+            }
+            console.log(productData);
+
+            const product = new ProductModel(productData);
+            await product.save();
+
+            res.json({success:true,message:"Product added successfully"}); 
 
         }catch(error){
             console.log(error);
