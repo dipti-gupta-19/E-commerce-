b2da01a4523cade80f004d61a215b8cf401edabe
dipti-gupta-19 <diptigupta10b6@gmail.com>
2025-10-22 21:24:47 +0530

    admin some part

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {assets} from "../assets/assets";
+const Navbar=()=>{
+    return(
+        <div>
+            <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
+            <button>Log Out</button>
+        </div>
+    );
+}
+export default Navbar;
