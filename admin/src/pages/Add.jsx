607f407d90e208b8ae37b59b44cf581d7ba5c61d
dipti-gupta-19 <diptigupta10b6@gmail.com>
2025-10-23 22:58:33 +0530

    doing at admin panel

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,63 @@
 import React from "react";
-const Add=()=>{
-  return(
-    <div></div>
+import { assets } from "../assets/assets";
+const Add = () => {
+  return (
+    <form className="flex flex-col w-full items-start gap-3" action="">
+      <div>
+        <p className="mb-2">Upload image</p>
+        <div className="flex gap-2">
+          <label htmlFor="image1">
+            <img className="w-20" src={assets.upload_area} alt="upload" />
+            <input type="file" id="image1" hidden />
+          </label>
+          <label htmlFor="image2">
+            <img className="w-20" src={assets.upload_area} alt="upload" />
+            <input type="file" id="image2" hidden />
+          </label>
+          <label htmlFor="image3">
+            <img className="w-20" src={assets.upload_area} alt="upload" />
+            <input type="file" id="image3" hidden />
+          </label>
+          <label htmlFor="image4">
+            <img className="w-20" src={assets.upload_area} alt="upload" />
+            <input type="file" id="image4" hidden />
+          </label>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <p className="mb-2">Product name</p>
+        <input className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Type here" required />
+      </div>
+      <div className="w-full">
+        <p className="mb-2">Product Description</p>
+        <textarea className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Write your content here" ></textarea>
+      </div>
+      <div>
+        <div>
+          <p>Product Category</p>
+          <select className="w-full px-3 py-2">
+            <option value="Men">Men</option>
+            <option value="Women">Women</option>
+            <option value="Kids">Kids</option>
+          </select>
+        </div>
+        <div>
+          <p>Sub Category</p>
+          <select className="w-full px-3 py-2">
+            <option value="Topwear">Topwear</option>
+            <option value="Bottomwear">Bottonwear</option>
+            <option value="Winterwear">Winterwear</option>
+          </select>
+        </div>
+        <div>
+          <p>
+            Product Price
+          </p>
+          <input type="number" placeholder="24"></input>
+        </div>
+      </div>
+    </form>
   )
 }
 export default Add;
